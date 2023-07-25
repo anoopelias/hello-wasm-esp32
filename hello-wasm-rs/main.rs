7706c5741208388a7262e17c8643d730d37c5379
Anoop Elias <anoopelias@gmail.com>
2023-07-26 04:53:28 +0530

    Split two params of printf

--- hello-wasm-rs/main.rs
+++ hello-wasm-rs/main.rs
@@ -32,17 +32,6 @@ fn start<T: Termination + 'static>(
     0
 }
 
-
-#[no_mangle]
-fn main() {
-    let to = "Hello rust world2\n";
-    let st: *const u8 = to.as_ptr() as *const u8;
-
-    unsafe {
-        printf(st, st);
-    }
-}
-
 #[no_mangle]
 fn __wasm_call_dtors() {}
 
@@ -51,4 +40,17 @@ fn __wasi_proc_exit(_param: i32) {}
 
 extern "C" {
     fn printf(input: *const u8, input2: *const u8) -> i32;
+}
+
+#[no_mangle]
+fn main() {
+    let hello = "Hello ";
+    let hello_ptr: *const u8 = hello.as_ptr() as *const u8;
+
+    let msg = "Rust World!\n";
+    let msg_ptr: *const u8 = msg.as_ptr() as *const u8;
+
+    unsafe {
+        printf(hello_ptr, msg_ptr);
+    }
 }
