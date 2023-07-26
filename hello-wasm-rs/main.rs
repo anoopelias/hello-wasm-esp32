1891466e9748fb4b2359d5e893b49eeabf789e4f
Anoop Elias <anoopelias@gmail.com>
2023-07-26 06:28:03 +0530

    Use puts instead of printf

--- hello-wasm-rs/main.rs
+++ hello-wasm-rs/main.rs
@@ -10,18 +10,15 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 extern "C" {
-    fn printf(input: *const u8, input2: *const u8) -> i32;
+    fn puts(input: *const u8) -> i32;
 }
 
 #[no_mangle]
 fn main() {
-    let hello = "Hello ";
+    let hello = "Hello Rust 5 World!";
     let hello_ptr: *const u8 = hello.as_ptr() as *const u8;
 
-    let msg = "Rust 4 World!\n";
-    let msg_ptr: *const u8 = msg.as_ptr() as *const u8;
-
     unsafe {
-        printf(hello_ptr, msg_ptr);
+        puts(hello_ptr);
     }
 }
