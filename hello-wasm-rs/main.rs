f62ab578a7f0c968883e7e0dc4d1c60f99685742
Anoop Elias <anoopelias@gmail.com>
2023-07-26 06:13:13 +0530

    Simplify rust implementation

--- hello-wasm-rs/main.rs
+++ hello-wasm-rs/main.rs
@@ -1,7 +1,6 @@
 
 #![no_std]
 #![no_main]
-#![feature(lang_items)]
 
 use core::panic::PanicInfo;
 
@@ -9,34 +8,6 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-pub trait Termination {
-    fn report(self) -> i32;
-}
-
-impl Termination for () {
-    fn report(self) -> i32 {
-        0
-    }
-}
-
-#[lang = "start"]
-fn start<T: Termination + 'static>(
-    _main: fn() -> T,
-    _argc: isize,
-    _argv: *const *const u8,
-    _uk: u8
-) -> isize {
-    // 42
-    main();
-    0
-}
-
-#[no_mangle]
-fn __wasm_call_dtors() {}
-
-#[no_mangle]
-fn __wasi_proc_exit(_param: i32) {}
 
 extern "C" {
     fn printf(input: *const u8, input2: *const u8) -> i32;
@@ -47,7 +18,7 @@ fn main() {
     let hello = "Hello ";
     let hello_ptr: *const u8 = hello.as_ptr() as *const u8;
 
-    let msg = "Rust World!\n";
+    let msg = "Rust 4 World!\n";
     let msg_ptr: *const u8 = msg.as_ptr() as *const u8;
 
     unsafe {
