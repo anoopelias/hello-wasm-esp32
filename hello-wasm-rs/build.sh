336248f613b525dc9811af64353741ee1cdf72df
Anoop Elias <anoopelias@gmail.com>
2023-07-27 11:35:34 +0530

    Add binarydump build to rust build

--- hello-wasm-rs/build.sh
+++ hello-wasm-rs/build.sh
@@ -1,6 +1,12 @@
 # 32kb stack size
 rustc -C link-self-contained=no -C link-args=-zstack-size=32768 -C link-args=--no-entry --target wasm32-wasi main.rs
 
+echo "Build binarydump tool .."
+rm -fr build && mkdir build && cd build
+cmake $WAMR_PATH/test-tools/binarydump-tool
+make
+cd ..
+
 echo "Generate test_wasm.h .."
 ./build/binarydump -o ../main/test_wasm.h -n wasm_test_file_interp main.wasm
 
