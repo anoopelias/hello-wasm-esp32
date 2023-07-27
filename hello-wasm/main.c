448e12285f81c9b0e09ff419fa9dbf3f23f0640c
Anoop Elias <anoopelias@gmail.com>
2023-07-27 15:14:37 +0530

    Removed copyright since this is a simple hello world now

--- hello-wasm/main.c
+++ hello-wasm/main.c
@@ -1,13 +1,6 @@
-/*
- * Copyright (C) 2019 Intel Corporation.  All rights reserved.
- * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     printf("Hello clang world!\n");
     return 0;
